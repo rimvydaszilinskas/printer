c337bbf61f2277c2560fe3d5a276f190abaf3b3d
Rimvydas Zilinskas <rilinskas@yahoo.com>
2019-02-18 15:08:48 +0100

    removed dpi

--- brother_ql_send.py
+++ brother_ql_send.py
@@ -4,7 +4,7 @@ from brother_ql.conversion import convert
 from brother_ql.raster import BrotherQLRaster
 from image_generate import create_card
 
-def print_label(text, qr, printer_identifier="/dev/usb/lp0", template="./templates/test.bmp", printer="QL-810W", cut=True, red=True, dpi_600=True, rotate="90"):
+def print_label(text, qr, printer_identifier="/dev/usb/lp0", template="./templates/test.bmp", printer="QL-810W", cut=True, red=True, dpi_600=True, rotate="0"):
     # create a card from the template
     card = create_card(target_file=template, text=text, qr=qr, save=False)
 
@@ -14,7 +14,7 @@ def print_label(text, qr, printer_identifier="/dev/usb/lp0", template="./templat
 
     images = [card]
 
-    instructions = convert(qlr=qlr, images=images, label="62", cut=cut, red=red, dpi_600=dpi_600, rotate=rotate)
+    instructions = convert(qlr=qlr, images=images, label="62", cut=cut, red=red, rotate=rotate)
     # get the backend identifier
     backend_identifier = guess_backend(printer_identifier)
 
